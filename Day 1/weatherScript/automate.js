b631bf4ada26e797752bb725786f47f60ab1da94
Muthu Aiswaryaa Swaminathan <withlove.aish@gmail.com>
2022-01-10 22:28:45 +0530

    aaaaaaaaaa

---
+++ Day 1/weatherScript/automate.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+const apiKey = require('./config');
+const cron = require('node-cron');
+cron.schedule("26 22 * * *", ()=>{
+    axios.get('http://api.openweathermap.org/data/2.5/weather?q=Madurai&appid='+apiKey)
+  .then(response => {
+    console.log("It's "+response.data.weather[0].main + ": "+response.data.weather[0].description);
+    console.log("temperature: "+response.data.main.temp);
+    console.log("pressure: "+response.data.main.pressure);
+    console.log("humidity: "+response.data.main.humidity);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+})
